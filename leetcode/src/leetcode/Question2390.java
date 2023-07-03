e4d990f8847b23cf31ea8a06a4fc6d26a82c88f5
wusy <906952910@qq.com>
2023-07-03 09:28:14 +0800

    Question2390

---
+++ leetcode/src/leetcode/Question2390.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+import java.util.Stack;
+
+/**
+ * 给你一个包含若干星号 * 的字符串 s 。
+ * 在一步操作中，你可以：
+ * 选中 s 中的一个星号。
+ * 移除星号 左侧 最近的那个 非星号 字符，并移除该星号自身。
+ * 返回移除 所有 星号之后的字符串
+ *
+ * @Author wushaoya
+ * @date 2023-07-03
+ * Time: 9:22
+ */
+public class Question2390 {
+
+    public String removeStars(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '*') {
+                stack.pop();
+            } else {
+                stack.push(s.charAt(i));
+            }
+        }
+        StringBuilder res = new StringBuilder();
+        stack.forEach(temp -> {
+            res.append(temp);
+        });
+        return res.toString();
+    }
+}
